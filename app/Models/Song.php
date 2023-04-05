3b10b84d05d9e67ddfb3fc1c4452e6e2dc6bfede
Jaden Baptista <jaden@baptista.dev>
2023-04-05 16:52:31 -0400

    fix

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
 
-class Song extends Model
-{
+class Song extends Model {
 	use Searchable;
     use HasFactory;
 }
